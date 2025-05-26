d0d87ade86f93dca2dff0cb43f275d36425a2b42
ActuallyWIKKO <dalineko@googlemail.com>
2025-05-26 22:43:57 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import {Header} from './components/Header/Header'
+import {Footer} from './components/Footer/Footer'
+import {SectionOne} from './components/Jokes/Jokes'
+
+function App() {
+
+  return (
+    <>
+     <Header />
+     <SectionOne />
+     <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Jokes/Jokes.tsx
@@ -0,0 +1,11 @@
+export const SectionOne:React.FC = () => {
+    return (
+        <section className="bg-base-100">
+        <div>
+            <p className="joke">Dad Joke goes here</p>
+            <button className="btn">Show a Dad Joke</button>    
+            {/* Add content for Section One */}
+                    </div>
+        </section>
+    );
+};
